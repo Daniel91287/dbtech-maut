dab18d1538e1c561d13f164e035b14a018c087b8
Daniel91287 <daimind87@icloud.com>
2025-10-23 10:12:54 +0200

    init db verbindung

--- javasrc/de/htwberlin/dbtech/utils/DbCredEmpty.java
+++ javasrc/de/htwberlin/dbtech/utils/DbCredEmpty.java
@@ -2,8 +2,8 @@ package de.htwberlin.dbtech.utils;
 
 public interface DbCredEmpty {
     String driverClass = "oracle.jdbc.driver.OracleDriver";
-    String url = "jdbc:oracle:thin:@aaa:1521:orcl";
-    String user = "uuu";
-    String password = "ppp";
+    String url = "jdbc:oracle:thin:@icla3lxc.f4.htw-berlin.de:1521:free";
+    String user = "u597686";
+    String password = "p597686";
     String schema = "uuu";
 }
